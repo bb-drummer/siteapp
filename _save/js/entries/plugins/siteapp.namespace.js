d345af92230c185e5eede469d3d218ecfc72ba81
Björn Bartels <me@bjoernbartels.earth>
2017-11-24 23:14:16 +0100

    removed obsolete files

--- _save/js/entries/plugins/siteapp.namespace.js
+++
@@ -1,5 +0,0 @@
-import { Siteapp } from './siteapp.core';
-import { Namespace } from '../../sys/siteapp.namespace';
-
-Siteapp.prototype.Namespace = Namespace;
-Siteapp.Namespace           = Siteapp.prototype.Namespace;
